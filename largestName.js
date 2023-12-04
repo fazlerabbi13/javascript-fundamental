60051ac107460bdd6a302810ba4804e2200a1f97
fazlerabbi13 <mohammad0132794@gmail.com>
2023-12-04 21:33:35 +0600

    finding larg name from an array

---
+++ largestName.js
@@ -0,0 +1,15 @@
+function largestName(names) {
+    let longgestName = names[0];
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        if(name.length > longgestName.length){
+            longgestName = name
+        }
+    }
+    return longgestName;
+}
+
+const namesList = ['nafi', 'robin', 'fazle rabbi', 'sagor', 'limon', 'sagor khan',
+    'rabbi', 'robi', 'nafi', 'hasan ali ', 'limon'];
+const largeName = largestName(namesList);
+console.log(largeName);
